552048470e6b915492972d03f24a698aec09b3aa
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-27 12:27:42 -0300

    test: dollar equality

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -8,4 +8,9 @@ describe('Dollar', () => {
     product = five.times(3)
     expect(product.amount).toEqual(15)
   })
+
+  it('should return true when dollars of the same value are comapred', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
 })

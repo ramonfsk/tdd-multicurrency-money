8e732377f918b9aed1c6e8353f1cb3447b3ce4c9
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-27 11:54:06 -0300

    test: remove dollar side-effects

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -2,8 +2,10 @@ import { Dollar } from '@/dollar'
 
 describe('Dollar', () => {
   it('should return 10 when five dollars are multiplied by two', () => {
-    const five = new Dollar(5)
-    five.times(2)
-    expect(five.amount).toEqual(10)
+    const five: Dollar = new Dollar(5)
+    let product: Dollar = five.times(2)
+    expect(product.amount).toEqual(10)
+    product = five.times(3)
+    expect(product.amount).toEqual(15)
   })
 })

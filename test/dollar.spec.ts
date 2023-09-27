7083b2934d6375abbc0714c12d2019f79d1d29b1
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-27 18:47:13 -0300

    test: make amount private

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -3,10 +3,8 @@ import { Dollar } from '@/dollar'
 describe('Dollar', () => {
   it('should return 10 when five dollars are multiplied by two', () => {
     const five: Dollar = new Dollar(5)
-    let product: Dollar = five.times(2)
-    expect(product.amount).toEqual(10)
-    product = five.times(3)
-    expect(product.amount).toEqual(15)
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
   })
 
   it('should return true when dollars of the same value are comapred', () => {

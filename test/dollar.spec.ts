20b70742cbf4fcabb3a498fd20b2ba624594975f
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-28 20:43:21 -0300

    test: base euro class

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -1,13 +1,19 @@
 import { Dollar } from '@/dollar'
+import { Euro } from '@/euro'
 
 describe('Dollar', () => {
-  it('should return 10 when five dollars are multiplied by two', () => {
+  it('should return 10 when five dollars are multiplied by two and 15 when multiplied by three', () => {
     const five: Dollar = new Dollar(5)
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
   })
 
-  it('should return true when dollars of the same value are comapred', () => {
+  it('should return 10 when five euros are multiplied by two and 15 when multiplied by three', () => {
+    expect(new Euro(5).equals(new Euro(5))).toBeTruthy()
+    expect(new Euro(5).equals(new Euro(6))).toBeFalsy()
+  })
+
+  it('should return true when dollars of the same value are compared', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
   })

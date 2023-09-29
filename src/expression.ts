a75775a1108c9ef1fa40923409c32e5c87dcdcd0
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-29 17:02:05 -0300

    feat: implement missing operations

--- src/expression.ts
+++ src/expression.ts
@@ -7,4 +7,6 @@ export interface Expression {
   reduce (bank: Bank, to: Currency): Money
   // eslint-disable-next-line no-unused-vars
   plus (addend: Expression): Expression
+  // eslint-disable-next-line no-unused-vars
+  times (multiplier: number): Expression
 }
--- src/sum.ts
+++ src/sum.ts
@@ -17,8 +17,11 @@ export class Sum implements Expression {
     return new Money(amount, to)
   }
 
-  // eslint-disable-next-line no-unused-vars
   plus (addend: Expression): Expression {
-    return null
+    return new Sum(this, addend)
+  }
+
+  times (multiplier: number): Expression {
+    return new Sum(this.augend.times(multiplier), this.addend.times(multiplier))
   }
 }

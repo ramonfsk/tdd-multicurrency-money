bdaf4c2f4315785281b1a7546ec3487ae59f21a7
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-29 14:39:43 -0300

    test: reduce from a sum and from a money

--- test/money.spec.ts
+++ test/money.spec.ts
@@ -2,6 +2,7 @@ import { Bank } from '@/bank'
 import { Currency } from '@/currency'
 import { Expression } from '@/expression'
 import { Money } from '@/money'
+import { Sum } from '@/sum'
 
 describe('Money', () => {
   it('should handle dollar multiplication', () => {
@@ -27,5 +28,26 @@ describe('Money', () => {
     const bank = new Bank()
     const reduced: Money = bank.reduce(sum, Currency.USD)
     expect(reduced.equals(Money.dollar(10))).toBeTruthy()
+  })
+
+  it('should return a sum when plus is used', () => {
+    const five = Money.dollar(5)
+    const result: Expression = five.plus(five)
+    const sum = result as Sum
+    expect(sum.augend).toEqual(five)
+    expect(sum.addend).toEqual(five)
+  })
+
+  it('should reduce from a sum', () => {
+    const sum: Expression = new Sum(Money.dollar(3), Money.dollar(4))
+    const bank = new Bank()
+    const result = bank.reduce(sum, Currency.USD)
+    expect(result).toEqual(Money.dollar(7))
+  })
+
+  it('should reduce from a money', () => {
+    const bank = new Bank()
+    const result: Money = bank.reduce(Money.dollar(1), Currency.USD)
+    expect(result).toEqual(Money.dollar(1))
   })
 })

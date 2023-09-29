4a05ba70b95b6260e563a84fce8ad4b4088af26d
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-28 21:45:06 -0300

    feat: create make factories for dollars and euros

--- src/euro.ts
+++
@@ -1,7 +0,0 @@
-import { Money } from './money'
-
-export class Euro extends Money {
-  times (multiplier: number): Euro {
-    return new Euro(this.amount * multiplier)
-  }
-}
--- src/money.ts
+++ src/money.ts
@@ -1,8 +1,12 @@
-export class Money {
+export abstract class Money {
   protected amount: number
 
-  constructor (amount: number) {
-    this.amount = amount
+  static dollar (amount: number): Dollar {
+    return new Dollar(amount)
+  }
+
+  static euro (amount: number): Euro {
+    return new Euro(amount)
   }
 
   equals (other: Money): boolean {
@@ -11,5 +15,29 @@ export class Money {
     }
 
     return this.amount === other.amount
+  }
+
+  abstract times (multiplier: number): Money
+}
+
+class Dollar extends Money {
+  constructor (amount: number) {
+    super()
+    this.amount = amount
+  }
+
+  times (multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier)
+  }
+}
+
+class Euro extends Money {
+  constructor (amount: number) {
+    super()
+    this.amount = amount
+  }
+
+  times (multiplier: number): Euro {
+    return new Euro(this.amount * multiplier)
   }
 }

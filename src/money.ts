ba96575dee09afe61e0b1e9c1ee9cf5a920dfe19
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-28 21:16:01 -0300

    feat: compare euros with dollars

--- src/money.ts
+++ src/money.ts
@@ -6,6 +6,10 @@ export class Money {
   }
 
   equals (other: Money): boolean {
+    if (this.constructor !== other.constructor) {
+      return false
+    }
+
     return this.amount === other.amount
   }
 }

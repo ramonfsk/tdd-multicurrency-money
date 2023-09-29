6d4c2fc8e2fbbac7b06a7ff95f4dc760b2851df7
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-29 10:10:22 -0300

    refactor: remove redudant tests

--- test/money.spec.ts
+++ test/money.spec.ts
@@ -1,3 +1,4 @@
+import { Currency } from '@/currency'
 import { Money } from '@/money'
 
 describe('Money', () => {
@@ -7,23 +8,14 @@ describe('Money', () => {
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
   })
 
-  it('should correctly handle euro multiplication', () => {
-    const five: Money = Money.euro(5)
-    expect(five.times(2).equals(Money.euro(10))).toBeTruthy()
-    expect(five.times(3).equals(Money.euro(15))).toBeTruthy()
-  })
-
   it('should correctly handle money equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
-    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy()
-    expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy()
     expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
-    expect(new Money(5, 'USD').equals(Money.dollar(5))).toBeTruthy()
   })
 
   it('should correctly handle currencies', () => {
-    expect(Money.dollar(1).currency()).toEqual('USD')
-    expect(Money.euro(1).currency()).toEqual('EUR')
+    expect(Money.dollar(1).currency()).toEqual(Currency.USD)
+    expect(Money.euro(1).currency()).toEqual(Currency.EUR)
   })
 })
